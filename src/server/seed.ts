b0e11a34de5027d2006a909565b2caa643d2c146
punkimen <punkimen666@gmail.com>
2026-05-17 17:09:44 +0000

    fix

--- src/server/seed.ts
+++ src/server/seed.ts
@@ -1,9 +1,10 @@
 import { prisma } from "./prisma/db";
-
+import { passwordController } from "./modules/auth/libs/passwordHasher";
 async function seed() {
   const email = process.env.ADMIN_LOGIN;
-  const password = process.env.ADMIN_PASSWORD;
-
+  const password = await passwordController.hash(process.env.ADMIN_PASSWORD);
+  console.log(process.env.ADMIN_PASSWORD)
+console.log(password, email)
   if (!email || !password) {
     throw new Error("ADMIN_LOGIN and ADMIN_PASSWORD must be set in .env");
   }
