f7604a2fae9ba91d34bef0e7ab93a06e42f0055e
Punkimen <punkimen666@gmail.com>
2026-06-14 22:51:14 +0300

    WIP

--- src/server/modules/email/controller.ts
+++ src/server/modules/email/controller.ts
@@ -1,4 +1,5 @@
 import { createTransport } from "nodemailer";
+import { AppError } from "../../utils/error";
 
 class SendEmailController {
   transporter = createTransport({
@@ -7,18 +8,29 @@ class SendEmailController {
     secure: false,
     auth: {
       type: "login",
-      user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASS,
+      user: process.env.SMTP_EMAIL ?? "",
+      pass: process.env.SMTP_PASS ?? "",
     },
   });
-  secondTransporter = createTransport(
-    `smtps://${process.env.SMTP_EMAIL}:${process.env.SMTP_PASS}@connect.smtp.bz/?pool=true`,
-  );
+
   verifyTransporter = async () => {
-    console.log(process.env.SMTP_EMAIL, process.env.SMTP_PASS);
     const res = await this.transporter.verify();
-    console.log({ res });
     return res;
+  };
+
+  sendMessage = async () => {
+    try {
+      const info = await this.transporter.sendMail({
+        from: `"Example Team" <${process.env.SMTP_EMAIL}>`,
+        to: "punkimen666@gmail.com, punkiment@yandex.ru",
+        subject: "Hello",
+        text: "Hello world",
+        html: "<b>Hello world!</b>",
+      });
+      console.log({ info });
+    } catch (e) {
+      throw new AppError(e instanceof Error ? e.message : String(e));
+    }
   };
 }
 
--- src/server/modules/email/routes.ts
+++ src/server/modules/email/routes.ts
@@ -3,4 +3,5 @@ import { sendEmailController } from "./controller";
 
 export const initEmailRoutes = (app: AppMethods) => {
   app.methodGet("/api/email/verify", sendEmailController.verifyTransporter);
+  app.methodGet("/api/email/testMessage", sendEmailController.sendMessage);
 };
